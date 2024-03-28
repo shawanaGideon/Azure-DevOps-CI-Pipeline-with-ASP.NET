33690c009b2f69da40003e32cd336c9033b260f2
shawanaGideon <gshawana@my.centennialcollege.ca>
2024-03-28 14:31:07 -0400

    Add program.cs

---
+++ program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace QuizFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Database access has already been done on April 14, 2023 by Gideon Shawana");
+
+            Console.WriteLine("You have Database Access");
+
+        }
+    }
+}
